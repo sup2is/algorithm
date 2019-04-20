0aba1f2027e9f43873f2cb9f3d51d9301c73e43e
sup2is <dev.sup2is@gmail.com>
2019-04-20 23:44:36 +0900

    1406 try

---
+++ beakjoon/beakjoon_1406/Main.java
@@ -0,0 +1,26 @@
+package beakjoon_1406;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		Stack<Integer> stack = new Stack<>();
+		
+		String str = bf.readLine();
+		int cnt = Integer.parseInt(bf.readLine());
+		
+		String command = "";
+		
+		for (int i = 0; i < cnt; i++) {
+			command = bf.readLine();
+			
+			
+		}
+	}
+}
