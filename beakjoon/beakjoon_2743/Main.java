19ef9fc70ffec9d3e44dcbbcc28847e19111fa99
sup2is <dev.sup2is@gmail.com>
2019-04-22 23:35:36 +0900

    2743 complete

---
+++ beakjoon/beakjoon_2743/Main.java
@@ -0,0 +1,16 @@
+package beakjoon_2743;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String str = bf.readLine();
+		System.out.println(str.length());
+	}
+	
+}
