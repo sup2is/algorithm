cd64dcfb4feb97da7f41a9a2f84966b0301e4156
sup2is <dev.sup2is@gmail.com>
2019-04-24 00:16:43 +0900

    11726 complete

---
+++ beakjoon/beakjoon_11726/Main.java
@@ -0,0 +1,24 @@
+package beakjoon_11726;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(bf.readLine());
+		int d[] = new int[N+1];
+		
+		d[0] = 1;
+		d[1] = 1;
+		
+		for (int i = 2; i <= N; i++) {
+			d[i] = d[i-1] + d[i-2];
+			d[i] %= 10007;
+		}
+
+		System.out.println(d[N]);
+	}
+}
