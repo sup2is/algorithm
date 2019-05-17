1872c6a8328ac6e0c0b6abcd578ccee2b4753852
sup2is <dev.sup2is@gmail.com>
2019-05-18 00:10:10 +0900

    11651 complete

---
+++ beakjoon/beakjoon_11651/Main.java
@@ -0,0 +1,39 @@
+package beakjoon_11651;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb = new StringBuffer();
+		int n = Integer.parseInt(br.readLine());
+		int[][] p = new int[n][2];
+		
+		String s[];
+		for (int i = 0; i < n; i++) {
+			s = br.readLine().split(" ");
+			p[i][0] = Integer.parseInt(s[0]);
+			p[i][1] = Integer.parseInt(s[1]);
+		}
+		
+		Arrays.sort(p , new Comparator<int[]>() {
+			@Override
+			public int compare(int[] x, int[] y) {
+				if(x[0] == y[0]) {
+					return Integer.compare(x[1], y[1]);
+				}
+				return Integer.compare(x[0], y[0]);
+			}
+		});
+
+		for (int i = 0; i < n; i++) {
+			sb.append(p[i][0] + " " + p[i][1] + "\n");
+		}
+		System.out.println(sb.toString());
+		
+	}
+}
