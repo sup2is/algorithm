5192f088ea7281a6cbc40c7ef617c9f1fabea517
sup2is <dev.sup2is@gmail.com>
2019-06-16 14:28:05 +0900

    2010 complete

---
+++ beakjoon/beakjoon_2010/Main.java
@@ -0,0 +1,19 @@
+package beakjoon_2010;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int sum = 0;
+		for (int i = 0; i < n - 1; i++) {
+			sum += Integer.parseInt(br.readLine())-1;
+		}
+		sum += Integer.parseInt(br.readLine());
+		System.out.println(sum);
+	}
+}
+
