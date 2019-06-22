ddffe610c3b55d9ae6c85c58587472a31a5501e0
sup2is <dev.sup2is@gmail.com>
2019-06-23 00:22:13 +0900

    2965 complete

---
+++ beakjoon/beakjoon_2965/Main.java
@@ -0,0 +1,17 @@
+package beakjoon_2965;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] arg) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		System.out.println(Math.max(b - a, c - b)-1);
+	}
+}
