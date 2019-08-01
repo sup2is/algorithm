d50c6b93554fd42f475eeb16618f251f62d8bf33
sup2is <dev.sup2is@gmail.com>
2019-08-02 00:13:04 +0900

    12790 complete

---
+++ beakjoon/beakjoon_12790/Main.java
@@ -0,0 +1,38 @@
+package beakjoon_12790;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int t = Integer.parseInt(br.readLine());
+		
+		int power;
+		int[] stat = new int[8];
+		
+		while(t-- > 0) {
+			
+			power = 0;
+			
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < 8; i++) {
+				stat[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			stat[0] = ((stat[0] + stat[4] > 1 ? stat[0] + stat[4] : 1) * 1);
+			stat[1] = ((stat[1] + stat[5] > 1 ? stat[1] + stat[5] : 1) * 5) ;
+			stat[2] = ((stat[2] + stat[6] > 0 ? stat[2] + stat[6] : 0) * 2);
+			stat[3] = ((stat[3] + stat[7]) * 2);
+			
+			for (int i = 0; i < 4; i++) {
+				power+= stat[i];
+			}
+			
+			System.out.println(power);
+		}
+	}
+}
