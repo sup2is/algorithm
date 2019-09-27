dde5a2258dee558d31d602b0f142a7238097cf2a
sup2is <dev.sup2is@gmail.com>
2019-09-28 00:32:21 +0900

    단어의 개수 https://www.acmicpc.net/problem/1152 Complete

--- beakjoon/beakjoon_1152/Main.java
+++ beakjoon/beakjoon_1152/Main.java
@@ -1,22 +1,16 @@
 package beakjoon_1152;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 	
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		System.out.println(st.countTokens());
 		
-		String str = br.readLine().trim();
-		String[] strArray = str.split(". ");
-		
-		bw.write(strArray.length + "");
-		bw.close();
 	}
 }
