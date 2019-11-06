616e4a5277836543f5e4c584460d38afffa31fea
sup2is <dev.sup2is@gmail.com>
2019-11-07 00:08:10 +0900

    손익분기점: https://www.acmicpc.net/problem/1712 Complete

---
+++ beakjoon/beakjoon_1712/Main.java
@@ -0,0 +1,23 @@
+package beakjoon_1712;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] arg) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		if(b < c) {
+			System.out.println((a / (c - b)) + 1);
+		}else {
+			System.out.println(-1);
+		}
+	}
+}
