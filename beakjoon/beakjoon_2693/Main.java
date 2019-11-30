f3f82537551517a63e4593c74dc6c158a21fda07
sup2is <dev.sup2is@gmail.com>
2019-11-30 11:24:03 +0900

    N번째 큰 수: https://www.acmicpc.net/problem/2693 Complete

---
+++ beakjoon/beakjoon_2693/Main.java
@@ -0,0 +1,30 @@
+package beakjoon_2693;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+   public static void main(String[] args) throws NumberFormatException, IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(br.readLine());
+      
+      while (n-- > 0) {
+         
+         int[] arr = new int[10]; 
+         
+         StringTokenizer st = new StringTokenizer(br.readLine());
+         
+         for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+         }
+         
+         Arrays.sort(arr);
+         
+         System.out.println(arr[7]);
+      }
+   }
+}
