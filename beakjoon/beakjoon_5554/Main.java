8a03d7aacfc5a785282857f7f7682cd1eb4cc01a
sup2is <30790184+sup2is@users.noreply.github.com>
2020-01-26 23:33:58 +0900

    심부름 가는 길: https://www.acmicpc.net/problem/5554 Complete

---
+++ beakjoon/beakjoon_5554/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int sum = 0;
+        
+        for(int i = 0; i < 4; i++){
+            sum += Integer.parseInt(br.readLine());
+        }
+        
+        System.out.println(sum / 60);
+        System.out.println(sum % 60);
+    }
+}
