3350e7e1259a33e842805fde39d6a419efec10af
sup2is <dev.sup2is@gmail.com>
2020-03-10 06:59:33 +0900

    N과 M(1): https://www.acmicpc.net/problem/15649 Complete

---
+++ beakjoon/beakjoon_15649/Main.java
@@ -0,0 +1,49 @@
+package beakjoon_15649;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+   static int[] arr;
+   static boolean[] visited;
+
+   public static void main(String[] args) throws NumberFormatException, IOException {
+
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int n = Integer.parseInt(st.nextToken());
+      int m = Integer.parseInt(st.nextToken());
+
+      arr = new int[n];
+      visited = new boolean[n];
+      for (int i = 0; i < n; i++) {
+         arr[i] = i + 1;
+      }
+
+
+      for (int i = 0; i < n; i++) {
+         visited[i] = true;
+         permutation(i, arr.length, m - 1, arr[i] + "");
+         visited[i] = false;
+      }
+   }
+
+   private static void permutation(int start, int n, int r, String s) {
+
+      if(r == 0) {
+         System.out.println(s);
+         return;
+      }
+
+      for (int i = 0; i < n; i++) {
+         if(!visited[i]){
+            visited[i] = true;
+            permutation(i, n, r - 1, s + " " + arr[i]);
+            visited[i] = false;
+         }
+      }
+   }
+}
