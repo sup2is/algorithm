c494ab5db5a0f61440aa5d3fa5aef032a213869f
sup2is <dev.sup2is@gmail.com>
2020-05-31 22:30:38 +0900

    N과 M (6): https://www.acmicpc.net/problem/15655 Complete

--- beakjoon/beakjoon_2579/Main.java
+++ beakjoon/beakjoon_2579/Main.java
@@ -3,28 +3,44 @@ package beakjoon_2579;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int d[] = new int[n+1];
-		int score[] = new int[n+1];
-		
-		for (int i = 0; i < n; i++) {
-			score[i] = Integer.parseInt(br.readLine());
-		}
-		
-		d[0] = score[0];
-		d[1] = Math.max(score[0] + score[1], score[1]);
-		d[2] = Math.max(score[0] + score[2], score[1] + score[2]);
-		
-		for (int i = 3; i < n; i++) {
-			d[i] = Math.max(d[i-2] + score[i], score[i-1] + score[i] + d[i-3]);
-		}
-		System.out.println(d[n-1]);
-		
-	}
-	
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int dp[] = new int[n + 1];
+
+        int arr[] = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        if(n == 1) {
+            System.out.println(arr[1]);
+            return;
+        }
+
+        if(n == 2) {
+            System.out.println(Math.max(arr[1] , arr[1] + arr[2]));
+            return;
+        }
+
+        dp[1] = arr[1];
+        dp[2] = Math.max(arr[1] , arr[1] + arr[2]);
+        dp[3] = Math.max(arr[1] + arr[3], arr[2] + arr[3]);
+
+
+        for (int i = 4; i <= n; i++) {
+            dp[i] = Math.max(dp[i-3] + arr[i-1] + arr[i], dp[i-2] + arr[i]);
+        }
+        System.out.println(dp[n]);
+
+    }
 }
