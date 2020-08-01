3b02a3503e4881a35511ae2103c44511148d92d9
sup2is <dev.sup2is@gmail.com>
2020-08-01 23:50:03 +0900

    알고리즘 수정

--- leetcode/kids_with_the_greatest_number_of_candies/Solution.java
+++ leetcode/kids_with_the_greatest_number_of_candies/Solution.java
@@ -1,25 +1,16 @@
 package kids_with_the_greatest_number_of_candies;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
 
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        List<Boolean> answers = new ArrayList<>();
-
         int max = Arrays.stream(candies).max().getAsInt();
-
-        for (int candy : candies) {
-            if(candy + extraCandies >= max) {
-                answers.add(true);
-            }else {
-                answers.add(false);
-            }
-        }
-
-        return answers;
+        return  Arrays.stream(candies)
+            .mapToObj(value -> value + extraCandies >= max)
+            .collect(Collectors.toList());
     }
 
 }
