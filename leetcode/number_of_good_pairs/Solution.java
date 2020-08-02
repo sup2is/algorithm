28565055f0939df84129c184f1cccb79315b012e
sup2is <dev.sup2is@gmail.com>
2020-08-02 10:21:24 +0900

    number-of-good-pairs: https://leetcode.com/problems/number-of-good-pairs/submissions/ Complete

---
+++ leetcode/number_of_good_pairs/Solution.java
@@ -0,0 +1,15 @@
+package number_of_good_pairs;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int cnt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if(nums[i] == nums[j]) cnt ++;
+            }
+        }
+        return cnt;
+    }
+}
