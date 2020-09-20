9b3efda58948232e62e08f27e2404bc480901043
sup2is <dev.sup2is@gmail.com>
2020-09-20 02:14:38 +0000

    reverse-string: https://leetcode.com/problems/reverse-string/ Complete

---
+++ leetcode/reverse-string/Solution.java
@@ -0,0 +1,17 @@
+//reverse-string: https://leetcode.com/problems/reverse-string/ Complete
+
+class Solution {
+    public void reverseString(char[] s) {
+        
+        int start = 0;
+        int end = s.length - 1;
+        
+        for(int i = 0; i < s.length / 2; i ++) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start ++;
+            end --;
+        }
+    }
+}
