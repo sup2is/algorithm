be88f5fa969df5a7354501dd02ec31751d8c388e
sup2is <dev.sup2is@gmail.com>
2020-11-01 14:39:38 +0900

    정렬: 버블소트

---
+++ basic/bublesort/Main.java
@@ -0,0 +1,25 @@
+package bublesort;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 9, 8, 5, 4, 2, 3, 7, 6};
+        System.out.println(Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
