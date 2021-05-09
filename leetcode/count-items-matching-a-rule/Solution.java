40f05d4e53e5a89edbe94f1aa2b9f391be75c727
Hyeon Seop Choi <30790184+sup2is@users.noreply.github.com>
2021-05-09 22:13:44 +0900

    count-items-matching-a-rule: https://leetcode.com/problems/count-items-matching-a-rule Complete

---
+++ leetcode/count-items-matching-a-rule/Solution.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/count-items-matching-a-rule
+
+class Solution {
+    
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+
+        int idx = 0;
+        switch(ruleKey) {
+            case "type": idx = 0; break;
+            case "color": idx = 1; break;
+            case "name": idx = 2; break;
+        }
+
+        final int finalIdx = idx;
+        return (int) items.stream()
+            .filter(strings -> strings.get(finalIdx).equals(ruleValue))
+            .count();
+    }
+
+}
