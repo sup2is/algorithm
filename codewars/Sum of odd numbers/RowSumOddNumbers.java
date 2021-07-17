f38a51080dbe5b7d9ac103515183b546e0418fc6
Hyeon Seop Choi <30790184+sup2is@users.noreply.github.com>
2021-07-17 11:11:11 +0900

    RowSumOddNumbers: https://www.codewars.com/kata/55fd2d567d94ac3bc9000064 Complete

---
+++ codewars/Sum of odd numbers/RowSumOddNumbers.java
@@ -0,0 +1,7 @@
+//RowSumOddNumbers: https://www.codewars.com/kata/55fd2d567d94ac3bc9000064 Complete
+
+class RowSumOddNumbers {
+    public static int rowSumOddNumbers(int n) {
+       return (int) Math.pow(n, 3);
+    }
+}
